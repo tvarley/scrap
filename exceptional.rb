c66750d81e7dea39fb7b8029df44c1b3318269e1
Tim Varley <tim.varley@gmail.com>
2016-05-20 10:34:31 -0400

    More exceptional

--- exceptional.rb
+++ exceptional.rb
@@ -1,10 +1,47 @@
 #!/usr/bin/env ruby
 
 def test_one
-  raise Exception.new("Why then")
-  # fail "Why now?"
+  # raise Exception.new("Why then")
+  fail 'Why now?'
 rescue => e
   puts "e[#{e}]"
 end
 
+def test_two
+  return 1 if 1 == 1
+  2
+end
+
+def test_three
+  test_hash = nil
+  puts 'Found' if test_hash.key?(:test)
+end
+
+def test_four
+  # fail "One"
+  begin
+    fail "Two"
+  rescue => e
+    puts "e2[#{e}]"
+  end
+rescue => e
+  puts "e[#{e}]"
+end
+
+def test_five
+  fail "WTF"
+  false
+rescue
+  nil
+end
+
 test_one
+puts test_two
+# test_three
+test_four
+test = test_five
+if test
+  puts 'trueish'
+else
+  puts 'unknownish'
+end
