80de583cd7a6161337e945408f8b492c0fdf15ef
Tim Varley <tim.varley@gmail.com>
2017-09-28 18:13:12 -0400

    dev

--- hours_count.rb
+++ hours_count.rb
@@ -1,26 +1,33 @@
 #!/usr/bin/env ruby
 
 require 'awesome_print'
+require 'byebug'
 require 'active_support/all'
 
 # DateTime extensions
-class DateTime
+class Time
   def to_whour
-    ((wday - 1) * 24 + hour)
+    (wday * 24) + hour
   end
 end
 
-def hours_count(*dates)
+def hours_count(dates)
   hour_accum = Array.new((24 * 7)) { 0 }
-  dates.each { |date| (date[0].to_whour..date[1].to_whour).each { |h| hour_accum[h] += 1 } }
-  ap hour_accum
+  byebug
+  dates.each{ |date| (date[0].to_whour..date[1].to_whour).each { |h| hour_accum[h] += 1 } }
   result = {}
-  %i(mon tue wed thu fri sat sun).each_with_index do |day, i|
-    ap day
-    ap i
-    ap ((i * 24)..((i * 24) + 23))
+  %i(sun mon tue wed thu fri sat).each_with_index do |day, i|
     result[day] = hour_accum[(i * 24)..((i * 24) + 23)]
   end
+  result
 end
 
-ap hours_count([5.hours.ago.to_datetime, 4.hours.ago.to_datetime])
+# ap hours_count([[5.hours.ago.to_datetime, 4.hours.ago.to_datetime]])
+
+dates = [
+  [23.hours.ago, 21.hours.ago],
+  [5.hours.ago, 2.hours.ago],
+  [1.week.ago - 5.hours, 1.week.ago - 2.hours]
+]
+
+ap hours_count(dates)
