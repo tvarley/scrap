79a4f30475508c03e1b63a028e9a217ea3c309ea
Tim Varley <tim.varley@gmail.com>
2018-02-04 12:55:56 -0500

    Time based windowing

--- windows_average.rb
+++ windows_average.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+require 'active_support/all'
 require 'awesome_print'
 require 'byebug'
 require 'benchmark'
@@ -17,5 +18,38 @@ def reduce_readings( data, number_values )
   average_readings
 end
 
-puts test_data.inspect
-ap reduce_readings(test_data,100)
+big_test_data = []
+end_time = Time.now
+start_time = end_time - 1.week
+curr_time = start_time
+while(curr_time < end_time)
+  big_test_data << {
+    timestamp: curr_time,
+    moisture: rand(11.0..12.0)
+  }
+  curr_time += 10.minutes + rand(1..360).seconds
+end
+
+Time.zone = 'Eastern Time (US & Canada)'
+
+def time_windows(data, window_size)
+  end_time = Time.zone.now
+  start_time = end_time - 1.week
+  windowed_data = []
+  curr_time = start_time
+  while( curr_time < end_time ) do
+    window_end = curr_time + window_size.minutes
+    rows_in_range = data.select{ |r| r[:timestamp] > curr_time && r[:timestamp] <= window_end }
+    rows_in_range_total = rows_in_range.inject(0.0){ |sum, j| sum += j[:moisture] }
+    windowed_data << {
+      timestamp: curr_time,
+      average: ( rows_in_range_total / rows_in_range.size )
+    }
+    curr_time = curr_time + window_size.minutes
+  end
+  windowed_data
+end
+
+windowme = time_windows(big_test_data, 60)
+ap big_test_data.count
+ap windowme.count
